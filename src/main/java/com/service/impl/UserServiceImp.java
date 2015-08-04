acc19d4cb529747b8b3ce87d812b39626539ab9d
勺子 <tangshaocheng@me.com>
2015-08-04 10:37:48 +0800

    A change

--- src/main/java/com/service/impl/UserServiceImp.java
+++ src/main/java/com/service/impl/UserServiceImp.java
@@ -2,7 +2,6 @@ package com.service.impl;
 
 import java.io.Serializable;
 
-
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheEvict;
@@ -34,33 +33,36 @@ public class UserServiceImp implements UserServiceI {
 	@Cacheable(value = "UserCache")
 	public TUserAppInfo findByName(String name) {
 		// TODO Auto-generated method stub
-		 logger.info("real querying user... {}"+ name);
-		 TUserAppInfo user = getFromDB(name);
-	        if (user==null) {
-	            throw new IllegalStateException(String.format("can not find account by account name : [%s]", name));
-	        }
+		logger.info("real querying user... {}" + name);
+		TUserAppInfo user = getFromDB(name);
+		if (user == null) {
+			throw new IllegalStateException(String.format(
+					"can not find account by account name : [%s]", name));
+		}
 
-	        return user;
+		return user;
 	}
 
 	private TUserAppInfo getFromDB(String name) {
-		logger.info("real querying db... {}"+name);
-		return new TUserAppInfo(name, name, name, null);
+		logger.info("real querying db... {}" + name);
+		return new TUserAppInfo(name, name, name, null, name);
 	}
 
 	@Override
-	@CacheEvict(value="UserCache",key="#u.getAppName()")
+	@CacheEvict(value = "UserCache", key = "#u.getAppName()")
 	public void updateUser(TUserAppInfo u) {
 		// TODO Auto-generated method stub
 		updateDB(u);
 	}
-	private void updateDB(TUserAppInfo u){
-		logger.info("real update db...{}"+u.getAppName());
+
+	private void updateDB(TUserAppInfo u) {
+		logger.info("real update db...{}" + u.getAppName());
 	}
+
 	@Override
-	@CacheEvict(value="UserCache" ,allEntries=true)
+	@CacheEvict(value = "UserCache", allEntries = true)
 	public void reload() {
 		// TODO Auto-generated method stub
 	}
-	
+
 }
